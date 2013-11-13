262fea464116bcf5c8cfbce92383151d88cc532c
Arjan Schouten <arjan-schouten@live.nl>
2013-11-13 22:36:11 +0100

    index changes

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+/*
+* This document is made by Tim and Arjan
+*
+* Here we go...
+*
+*/
+
+//put here the directory of the application map
+$config['application'] = '../application';
+
+//put here the directory of the system map
+$config['system'] = '../system';
+
+//---------------------------------------------------------
+/*
+* Don't make changes below
+*
+*/
+
+
+?>
